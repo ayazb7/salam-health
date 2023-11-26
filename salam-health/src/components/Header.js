280106af35ca3575d13dd49856cf18d4b2ee2aaf
mfd1g21 <mfd1g21@soton.ac.uk>
2023-11-26 18:13:00 +0000

    first edit

---
+++ salam-health/src/components/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Medical Health App</h1>
+      {/* Add navigation links or other header content as needed */}
+    </header>
+  );
+};
+
+export default Header;
