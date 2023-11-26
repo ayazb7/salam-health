87dd812a43b7cbc0b0d53b8541344df0fa320c20
ayazb <mab1g21@soton.ac.uk>
2023-11-26 18:29:55 +0000

    nav bar + routing

--- salam-health/src/components/Header.js
+++ salam-health/src/components/Header.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
