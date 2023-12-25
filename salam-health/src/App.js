45972ded9fc4af05a0b5af14427bd56a7664f237
ayazb7 <ayazbaig@hotmail.co.uk>
2023-12-25 23:52:35 +0000

    changes to home page

--- salam-health/src/App.js
+++ salam-health/src/App.js
@@ -1,26 +1,64 @@
-// App.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import LogoImage from './images/Logo.png'
+import LogoImage from './images/Logo3.png';
 import './App.css';
 
 function App() {
+  // State for scroll position and navbar background color
+  const [scrollY, setScrollY] = useState(0);
+  const [navbarBackground, setNavbarBackground] = useState('transparent');
+
+  // Function to handle scroll event
+  const handleScroll = () => {
+    // Get the current scroll position
+    const currentScrollY = window.scrollY;
+
+    // Update the scrollY state
+    setScrollY(currentScrollY);
+
+    // Define the threshold for background color change
+    const threshold = 50; // Adjust this threshold as needed
+
+    // Update the navbar background color based on the scroll position
+    if (currentScrollY > threshold) {
+      setNavbarBackground('rgba(17, 11, 39, 0.9)');
+    } else {
+      setNavbarBackground('transparent');
+    }
+  };
+
+  // Add a scroll event listener when the component mounts
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <Router>
       <div>
         <nav className="navbar">
-          <ul className="nav-list">
-          <li className="logo-container">
-              {/* Use the img element for the logo with the "logo" class */}
-              <img src={LogoImage} alt="Logo" className="logo" />
-            </li>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
+          <div className="nav-item-left">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+          </div>
+          <Link to="/" className="nav-brand">
+            <img src={LogoImage} alt="Logo" className="logo" />
+            {/* <img src={TitleImage} alt="Title" className="title" /> */}
+          </Link>
+          {/* <a href="/" className="nav-brand">Salam Health</a> */}
+          <div className="nav-item-right">
+            <a href="/contact">Contact</a>
+            <div className="book-now">
+              <p href="/book" className="book-now-text">Book Now</p>
+            </div>
+          </div>
         </nav>
 
         <Routes>
@@ -34,3 +72,4 @@ function App() {
 }
 
 export default App;
+
--- salam-health/src/components/Home.js
+++ salam-health/src/components/Home.js
@@ -1,12 +1,20 @@
 // Home.js
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css'; // Import your CSS file
+import CuppingImage from '../images/Salam Health Cupping.webp';
+import MassageImage from '../images/Salam Health Massage.webp';
+import GrastonImage from '../images/graston.webp';
 
 const Home = () => {
+  const [startAnimation, setStartAnimation] = useState(false);
+
   useEffect(() => {
+    const timer = setTimeout(() => {
+      setStartAnimation(true);
+    }, 350);
+
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
-        console.log(entry);
         if (entry.isIntersecting) {
           entry.target.classList.add('show');
         } else {
@@ -19,18 +27,39 @@ const Home = () => {
     hiddenElements.forEach((el) => observer.observe(el));
 
     // Clean up the observer on component unmount
-    return () => observer.disconnect();
+    return () => {
+      clearTimeout(timer);
+      observer.disconnect();
+    };
   }, []); // Empty dependency array ensures this effect runs once after initial render
 
   return (
-    <div className="home-background">
-      <h2 className="hidden">
-        Salam Health
-      </h2>
-  
+    <div className={`home-background ${startAnimation ? 'start-animation' : 'initial-hidden'}`}>
+      <div className="centered-container hidden">
+
+        <h3 className="home-description">
+          Holistic healing and natural therapies
+        </h3>
+        <h3 className="home-description-bottom">
+          specially tailored just for you.
+        </h3>
+
+        <div className="images">
+          <img src={CuppingImage} alt="Cupping" className="home-image" />
+          <img src={MassageImage} alt="Massage" className="home-image" />
+          <img src={GrastonImage} alt="Graston" className="home-image" />
+        </div>
+
+        <h5 className="book-text">
+          Book your free health consultation
+        </h5>
+
+        <div className="book-now-button">
+          <p href="/book" className="book-now-text">Book Now</p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Home;
-
