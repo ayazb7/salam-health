d26722f080d82a8620e63c1bacb0a10647077c3a
ayazb7 <ayazbaig@hotmail.co.uk>
2024-02-04 15:59:57 +0000

    changes

--- salam-health/src/components/ContactCard.js
+++ salam-health/src/components/ContactCard.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import contactImage from '../images/contact-image.png';
+import arrow_right from '../images/chevron-right.png';
 
 function ContactCard() {
 
   return (
     <div className="contact-card">
-        <div className='vertical-start input-padding'>
+        <div className='vertical-start input-padding gap-1'>
           <h4>Contact Us</h4>
           <div className='inputs-container'>
             <div className='horizontal-input'>
@@ -17,6 +18,21 @@ function ContactCard() {
             <input type="text" placeholder="Email" className="styled-input"/>
 
             <textarea type="text" placeholder="Message" className="styled-input large-input"/>
+          </div>
+
+          <div className='horizontal'>
+            <button className="send-message">
+              Get Started
+              <div className='circle'>
+                <img src={arrow_right}/>
+              </div>
+            </button>
+
+            <button className="send-message">
+              <div className='circle'>
+                <img src={arrow_right}/>
+              </div>
+            </button>
           </div>
         </div>
 
