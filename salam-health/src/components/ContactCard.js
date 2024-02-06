03b6ba1b49b7357f88a9afcfc3d1aa1fd1ab3a5b
ayazb7 <ayazbaig@hotmail.co.uk>
2024-02-06 08:07:53 +0000

    thank you message

--- salam-health/src/components/ContactCard.js
+++ salam-health/src/components/ContactCard.js
@@ -4,12 +4,14 @@ import emailjs from 'emailjs-com';
 import contactImage from '../images/contact-image.png';
 import arrow_right from '../images/chevron-right.png';
 import phone from '../images/phone.png';
+import Logo from '../images/Logo.png';
 
 function ContactCard(props) {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [emailSent, setEmailSent] = useState(false);
   const [validationMessage, setValidationMessage] = useState('');
   const form = useRef();
 
@@ -27,6 +29,7 @@ function ContactCard(props) {
       .then(
         (result) => {
           console.log('Email sent successfully:', result.text);
+          setEmailSent(true); 
           // Clear the form fields after successful submission
           setFirstName('');
           setLastName('');
@@ -41,68 +44,81 @@ function ContactCard(props) {
 
   return (
     <div className="contact-card" style={props.style}>
-      <form ref={form} onSubmit={sendEmail} className='vertical-start input-padding gap-1'>
-        <h4>Contact Us</h4>
-        <div className='inputs-container'>
-          <div className='horizontal-input'>
-            <input
-              type="text"
-              autoComplete="given-name"
-              placeholder="First Name"
-              className="styled-input"
-              name="firstName"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              required
-            />
-            <input
-              type="text"
-              autoComplete="family-name"
-              placeholder="Last Name"
-              className="styled-input"
-              name="lastName"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              required
-            />
+      {
+        !emailSent ? (
+            <form ref={form} onSubmit={sendEmail} className='vertical-start input-padding gap-1'>
+          <h4>Contact Us</h4>
+          <div className='inputs-container'>
+            <div className='horizontal-input'>
+              <input
+                type="text"
+                autoComplete="given-name"
+                placeholder="First Name"
+                className="styled-input"
+                name="firstName"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+              <input
+                type="text"
+                autoComplete="family-name"
+                placeholder="Last Name"
+                className="styled-input"
+                name="lastName"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </div>
+            <div className='horizontal-input'>
+              <input
+                type="email"
+                autoComplete="email"
+                placeholder="Email"
+                className="styled-input"
+                name = 'email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className='horizontal-input'>
+              <textarea
+                placeholder="Message"
+                className="styled-input large-input"
+                name='message'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              />
+            </div>
           </div>
-          <div className='horizontal-input'>
-            <input
-              type="email"
-              autoComplete="email"
-              placeholder="Email"
-              className="styled-input"
-              name = 'email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+
+          <div className='horizontal'>
+            <button type="submit" className="send-message">
+              Send message
+              <img src={arrow_right} alt="Arrow right"/>
+            </button>
+
+            <a href="tel:+447459150327" className="circle-button">
+              <img src={phone} alt="Call" />
+            </a>
           </div>
-          <div className='horizontal-input'>
-            <textarea
-              placeholder="Message"
-              className="styled-input large-input"
-              name='message'
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-            />
+        </form>
+        ) : (
+          <div className="thank-you-message">
+            <img src={Logo} alt="Logo" className="thank-you-logo" />
+            <h4 style={{ fontSize: '28px', marginBottom: '1rem' }}>Thank you for your message!</h4>
+            <p style={{ fontSize: '18px', marginTop: '0rem' }}>We will make sure to respond as soon as possible.</p>
+            <a href="tel:+447459150327" className="circle-button">
+              <img src={phone} alt="Call" />
+            </a>
           </div>
-        </div>
+        )
+      }
 
-        <div className='horizontal'>
-          <button type="submit" className="send-message">
-            Send message
-            <img src={arrow_right} alt="Arrow right"/>
-          </button>
-
-          <a href="tel:+447459150327" className="circle-button">
-            <img src={phone} alt="Call" />
-          </a>
-        </div>
-      </form>
-
-      <div className="contact-image">
+      <div className="contact-image" style={{ marginLeft: emailSent ? '0' : '' }}>
         <img src={contactImage} alt="Contact"/>
       </div>
     </div>
