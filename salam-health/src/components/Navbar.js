072b08668ef9412b3e3331a2250817a3757e0e2c
karim elsayed <kwsa1g20@soton.ac.uk>
2024-02-07 22:05:16 +0000

    "sidebar"

--- salam-health/src/components/Navbar.js
+++ salam-health/src/components/Navbar.js
@@ -2,11 +2,19 @@
 import React, { useState, useEffect, } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LogoImage from '../images/full_logo.png';
+import { AnimatePresence, motion } from 'framer-motion';
+import { ReactComponent as MySVG } from '../menu.svg';
+import { ReactComponent as MyCloseSVG } from '../close.svg';
 // import { slide as Menu } from 'react-burger-menu';
 
 function Navbar() {
   const navigate = useNavigate(); // Initialize the navigate function here
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  const [CloseisClicked, setCloseIsClicked] = useState(false);
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   const handleButtonClick = () => {
     navigate('/book'); // Navigate to /book when the button is clicked
   };
@@ -23,12 +31,33 @@ function Navbar() {
         <button className="book-now" onClick={handleButtonClick}>
           Book Now
         </button>
+        
       </div>
+      <MySVG className={`menu-mobile-only ${isClicked ? 'clicked' : ''}`} onClick={()=>{setIsClicked(prevState => !prevState); handleMenuToggle(); }}  />
       {/* <Menu>
         <a id="home" className="menu-item" href="/">Home</a>
         <a id="about" className="menu-item" href="/about">About</a>
         <a id="contact" className="menu-item" href="/contact">Contact</a>
       </Menu> */}
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            className="burger-menu"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{ duration: 0.3 }}
+          >
+            
+            <MyCloseSVG className="menu-mobile-only-close" onClick={handleMenuToggle}  />
+            
+            <a href="/about">About Us</a>
+            <button className="book-now" onClick={handleButtonClick}>
+              Book Now
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 }
