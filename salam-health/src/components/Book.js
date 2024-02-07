f1bcec18963b2f90c8d3aea62c5d175773060966
ayazb7 <ayazbaig@hotmail.co.uk>
2024-02-07 14:16:40 +0000

    more images + texts

--- salam-health/src/components/Book.js
+++ salam-health/src/components/Book.js
@@ -6,6 +6,8 @@ import FAQ from './FAQ.js';
 import SportsMassage from '../images/sports_massage.png';
 import CuppingTherapy from '../images/cupping-therapy.png';
 import ActiveCupping from '../images/active_cupping2.png';
+import LiteDetox from '../images/lite_detox.png';
+import FullDetox from '../images/full_detox.png';
 
 function Book() {
     const [startAnimation, setStartAnimation] = useState(false);
@@ -45,14 +47,14 @@ function Book() {
           <h3 className="home-text" style={{ fontSize: '32px'}}>Stand Alone Treatments</h3>
 
           <div className='horizontal column more-gap' style={{ alignItems: 'stretch'}}>
-            <BookingCard className="cards" title= "Sports Massage" image={SportsMassage} description="The process of using suction cups on the skin to draw out a small amount of blood, promoting detoxification and stimulating healing." price="30" duration="30 min"/>
-            <BookingCard className="cards" title= "Active Cupping" image={ActiveCupping} description="Combines cupping therapy with physical movement or exercise to target specific areas, enhance blood flow, and improve muscle recovery and flexibility." price="30" duration="30 min"/>
-            <BookingCard className="cards" title= "Hijamah Wet Cupping" image={CuppingTherapy} description="HEALTH CONSULTATION , SPORTS MASSAGE , INFRARED DEEP HEAT , WET CUPPING , GRASTON THERAPY" price="45" duration="45 min"/>
+            <BookingCard className="cards" title= "Sports Massage" image={SportsMassage} description="Unlock your full potential and embrace a pain-free, active lifestyle with our transformative sports massage therapy, designed to nurture your body, mind, and spirit." price="30" duration="30 min" learnMorePath="/sports-massage"/>
+            <BookingCard className="cards" title= "Active Cupping" image={ActiveCupping} description="Combines cupping therapy with physical movement or exercise to target specific areas, enhance blood flow, and improve muscle recovery and flexibility." price="30" duration="30 min" learnMorePath="/cupping-therapy"/>
+            <BookingCard className="cards" title= "Hijamah Wet Cupping" image={CuppingTherapy} description="The process of using suction cups on the skin to draw out a small amount of blood, promoting detoxification and stimulating healing." price="45" duration="45 min" learnMorePath="/cupping-therapy"/>
           </div>
       </div>
 
       <div className='centered-container-no-height hidden'>
-        <div className='rectangle-fw blue' style={{ height: '70vh' }}> 
+        <div className='rectangle-fw blue' style={{ height: '77.5vh' }}> 
           <p className='center testimonial-title' style={{ marginBottom: '1rem'}} >
             Packages & Bundles
           </p>
@@ -61,9 +63,8 @@ function Book() {
           </p>
 
           <div className='horizontal column more-gap' style={{ alignItems: 'stretch'}}>
-            <BookingCard className="cards" title= "Hijamah Wet Cupping" image={CuppingTherapy} description="HEALTH CONSULTATION , SPORTS MASSAGE , INFRARED DEEP HEAT , WET CUPPING , GRASTON THERAPY" price="63" duration="45 min"/>
-            <BookingCard className="cards" title= "Hijamah Wet Cupping" image={ActiveCupping} description="HEALTH CONSULTATION , SPORTS MASSAGE , INFRARED DEEP HEAT , WET CUPPING , GRASTON THERAPY" price="63" duration="45 min"/>
-            <BookingCard className="cards" title= "Hijamah Wet Cupping" image={CuppingTherapy} description="HEALTH CONSULTATION , SPORTS MASSAGE , INFRARED DEEP HEAT , WET CUPPING , GRASTON THERAPY" price="63" duration="45 min"/>
+            <BookingCard className="cards" title= "Lite Detox" image={LiteDetox} description={["Consultation", "Infrared Deep Tissue Heat", "Hijama Wet Cupping"]} price="45" duration="45 min" style={{ height: '90%' }}/>
+            <BookingCard className="cards" title= "Full Detox & Sports Therapy" image={FullDetox} description={["Consultation", "Sports Massage", "Infrared Deep Tissue Heat", "Hijama Wet Cupping"]} price="70" duration="1 hour" style={{ height: '90%' }}/>
           </div>
         </div>
 
--- salam-health/src/components/BookingCard.js
+++ salam-health/src/components/BookingCard.js
@@ -1,23 +1,39 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import CardImage from '../images/CardImage.png';
 import arrow_right from '../images/chevron-right.png';
 
-export default function Card(props){
+export default function BookingCard(props){
     const handleButtonClick = () => {
         window.open('https://www.fresha.com/a/salam-health-southampton-uk-19-oxford-street-khby691r?_gl=1*1xc7coa*_ga*MTgzNTI0NTc3OS4xNzA2Nzg4OTc4*_ga_SMQNG7NE8C*MTcwNjk3NTE1My41LjEuMTcwNjk3OTkzOS40My4wLjA.&pId=973826', '_blank');
     };
-    
-    const learnMoreClick = () =>{
-        console.log("clicked learn more")
-    }
+
+    const navigate = useNavigate();
+
+    const handleLearnMore = () => {
+        navigate(props.learnMorePath, { replace: false, state: {} });
+        window.scrollTo(0, 0);
+      };
+
+    const renderDescription = () => {
+        if (Array.isArray(props.description)) {
+            // Render as list if description is an array
+            return props.description.map((item, index) => (
+                <p key={index} className="treatment-info" style={{ textAlign: 'start', marginBottom: '0', marginTop: '0.5rem' }}>{item}</p>
+            ));
+        } else {
+            // Render as paragraph if description is a string
+            return <p className="treatment-info" style={{ textAlign: 'start' }}>{props.description}</p>;
+        }
+    };
 
     return(
-        <div className="treatment-card">
+        <div className="treatment-card" style={ props.style }>
             <img src={props.image} alt='treatment image'/>
 
-            <div className="treatment-text">
+            <div className="treatment-text" style={{ width: Array.isArray(props.description) ? '90%' : 'initial' }}>
                 <p className="treatment-name" style={{ textAlign: 'start', fontWeight: '700' }}>{props.title}</p>
-                <p className="treatment-info" style={{ textAlign: 'start' }}>{props.description}</p>
+                {renderDescription()}
 
                 <p className="treatment-info" style={{ textAlign: 'start', marginBottom: '0', fontWeight: '700' }}>£{props.price}</p>
                 <p className="treatment-info" style={{ textAlign: 'start', marginTop: '0', marginBottom: '0' }}>{props.duration}</p>
@@ -28,7 +44,7 @@ export default function Card(props){
                     Book
                 </button>
 
-                <div className='learn-more' style={{ justifyContent: 'space-around', width: '8.5rem' }}>
+                <div className='learn-more' style={{ justifyContent: 'space-around', width: '8.5rem' }} onClick={handleLearnMore}>
                     <p style={{ fontWeight: '600', fontSize: '16px' }}>Learn More</p>
                     <div className='circle' style={{ width: '30px', height: '30px' }}>
                         <img src={arrow_right} style={{ width: '25px', height: '25px' }}/>
