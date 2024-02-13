3b1604beac5a09f67bb124fdd9a45164e96cf45f
ayazb7 <ayazbaig@hotmail.co.uk>
2024-02-13 16:11:24 +0000

    fixes

--- salam-health/src/components/Book.js
+++ salam-health/src/components/Book.js
@@ -46,7 +46,7 @@ function Book() {
           </p>
           <h3 className="home-text" style={{ fontSize: '32px'}}>Stand Alone Treatments</h3>
 
-          <div className='horizontal column more-gap' style={{ alignItems: 'stretch', height: '65vh' }}>
+          <div className='horizontal column more-gap mobile-no-height' style={{ alignItems: 'stretch', height: '65vh' }}>
             <BookingCard className="cards" title= "Sports Massage" image={SportsMassage} description="Unlock your full potential and embrace a pain-free, active lifestyle with our transformative sports massage therapy, designed to nurture your body, mind, and spirit." price="30" duration="30 min" learnMorePath="/sports-massage"/>
             <BookingCard className="cards" title= "Active Cupping" image={ActiveCupping} description="Combines cupping therapy with physical movement or exercise to target specific areas, enhance blood flow, and improve muscle recovery and flexibility." price="30" duration="30 min" learnMorePath="/cupping-therapy"/>
             <BookingCard className="cards" title= "Hijamah Wet Cupping" image={CuppingTherapy} description="The process of using suction cups on the skin to draw out a small amount of blood, promoting detoxification and stimulating healing." price="45" duration="45 min" learnMorePath="/cupping-therapy"/>
